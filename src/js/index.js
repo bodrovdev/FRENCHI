9f08b644d46507c2c36ab803b68a02cf617c6dcd
Igor Bodrov <bodrovigor02@gmail.com>
2023-08-22 10:45:17 +0300

    refactor: 💡 initial commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// ? --- Отключение подсветки ошибок в инпутах
+window.addEventListener('load', () => {
+  if (!(document.querySelectorAll('input') === null)) {
+    document.querySelectorAll('input').forEach((input) => {
+      input.setAttribute('spellcheck', 'false');
+    })
+  }
+})
+
+// ? --- Показать страницу только после загрузки
+window.addEventListener('load', () => {
+  document.body.removeAttribute('style');
+})
