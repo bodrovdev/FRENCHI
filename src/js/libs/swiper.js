aadcbc99413e7002d509327bfb89c7c2fbce177e
Igor Bodrov <bodrovigor02@gmail.com>
2023-08-23 22:24:25 +0300

    fix: 🐛 bugfix

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,61 +1,106 @@
 import Swiper from 'swiper/bundle';
 
 // ! --- Слайдер в хедере
-// let heading_slider = new Swiper(".heading__slider", {
-//   direction: "horizontal",
-//   slidesPerView: 1,
+let heading_slider = new Swiper(".heading__slider", {
+  direction: "horizontal",
+  slidesPerView: 1,
 
-//   pagination: {
-//     el: ".heading__slider-pagination",
-//     clickable: true,
-//   },
-// });
+  pagination: {
+    el: ".heading__slider-pagination",
+    clickable: true,
+  },
+});
 
 // ! --- Слайдер первого каталога на главной
-// var index_catalogue_slider_init = false;
+var index_catalogue_slider_init = false;
 
-// function index_catalogue_slider() {
-//   if (window.innerWidth <= 1024) {
-//     if (!index_catalogue_slider_init) {
-//       index_catalogue_slider_init = true;
+function index_catalogue_slider() {
+  if (window.innerWidth <= 1024) {
+    if (!index_catalogue_slider_init) {
+      index_catalogue_slider_init = true;
 
-//       var index_catalogue_slider = new Swiper(".index-catalogue__slider", {
-//         direction: "horizontal",
-//         spaceBetween: 15,
-//         slidesPerView: "auto",
+      var index_catalogue_slider = new Swiper(".index-catalogue__slider", {
+        direction: "horizontal",
+        spaceBetween: 15,
+        slidesPerView: "auto",
 
-//         pagination: {
-//           el: ".index-catalogue__slider-pagination",
-//           type: "progressbar",
-//           clickable: true,
-//         },
+        pagination: {
+          el: ".index-catalogue__slider-pagination",
+          type: "progressbar",
+          clickable: true,
+        },
 
-//       });
-//     }
-//   } else if (index_catalogue_slider_init) {
-//     index_catalogue_slider.destroy();
-//     index_catalogue_slider_init = false;
-//   }
-// }
-// index_catalogue_slider();
-// window.addEventListener("resize", index_catalogue_slider);
+      });
+    }
+  } else if (index_catalogue_slider_init) {
+    index_catalogue_slider.destroy();
+    index_catalogue_slider_init = false;
+  }
+}
+index_catalogue_slider();
+window.addEventListener("resize", index_catalogue_slider);
 
-// ! --- Слайдер второго каталога на главной
-// let index_catalogue_slider_second = new Swiper(".index-catalogue-goods__slider", {
-//   direction: "horizontal",
-//   slidesPerView: 5,
-//   spaceBetween: 48,
+// ! --- Слайдеры второго каталога на главной
+let index_catalogue_slider_second_1 = new Swiper(".index-catalogue-goods__slider--1", {
+  direction: "horizontal",
+  slidesPerView: 5,
+  spaceBetween: 48,
 
-//   navigation: {
-//     nextEl: '.index-catalogue-goods__nav-arrow--next',
-//     prevEl: '.index-catalogue-goods__nav-arrow--prev',
-//   },
+  navigation: {
+    nextEl: '.index-catalogue-goods__nav-arrow--next',
+    prevEl: '.index-catalogue-goods__nav-arrow--prev',
+  },
 
-//   pagination: {
-//     el: ".index-catalogue-goods__slider-pagination",
-//     clickable: true,
-//   },
-// });
+  pagination: {
+    el: ".index-catalogue-goods__slider-pagination",
+    clickable: true,
+  },
+});
+let index_catalogue_slider_second_2 = new Swiper(".index-catalogue-goods__slider--2", {
+  direction: "horizontal",
+  slidesPerView: 5,
+  spaceBetween: 48,
+
+  navigation: {
+    nextEl: '.index-catalogue-goods__nav-arrow--next',
+    prevEl: '.index-catalogue-goods__nav-arrow--prev',
+  },
+
+  pagination: {
+    el: ".index-catalogue-goods__slider-pagination",
+    clickable: true,
+  },
+});
+let index_catalogue_slider_second_3 = new Swiper(".index-catalogue-goods__slider--3", {
+  direction: "horizontal",
+  slidesPerView: 5,
+  spaceBetween: 48,
+
+  navigation: {
+    nextEl: '.index-catalogue-goods__nav-arrow--next',
+    prevEl: '.index-catalogue-goods__nav-arrow--prev',
+  },
+
+  pagination: {
+    el: ".index-catalogue-goods__slider-pagination",
+    clickable: true,
+  },
+});
+let index_catalogue_slider_second_4 = new Swiper(".index-catalogue-goods__slider--4", {
+  direction: "horizontal",
+  slidesPerView: 5,
+  spaceBetween: 48,
+
+  navigation: {
+    nextEl: '.index-catalogue-goods__nav-arrow--next',
+    prevEl: '.index-catalogue-goods__nav-arrow--prev',
+  },
+
+  pagination: {
+    el: ".index-catalogue-goods__slider-pagination",
+    clickable: true,
+  },
+});
 
 // ? Мобильный слайдер
 // var mobile_slider_init = false;
